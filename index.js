e34a99b0c27b85950040163aa87f9dc229bd8c64
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-21 19:00:37 +0700

    feat: Add event emitter with listener function

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ const message = (name) => {
 }
 
 message('yoyo');
+
+// Event Emitter Test Site
+const { EventEmitter } = require('events');
+const myEventEmitter = new EventEmitter();
+
+const makeCoffee = ({coffeeName}) => {
+    console.log(`Kopi ${coffeeName} telah selesai dibuat!`);
+}
+
+const makeBill = ({coffeePrice}) => {
+    console.log(`Bill sebesar $${coffeePrice} telah selesai dibuat!`);
+}
+
+const onCoffeeOrderedListener = ({coffeeName, coffeePrice}) => {
+    makeCoffee({coffeeName});
+    makeBill({coffeePrice});
+}
+
+myEventEmitter.on('make-a-order', onCoffeeOrderedListener);
+
+let allCoffeePrice = 20;
+
+for (let timer = 0; timer <= 100; timer++){
+    console.log(timer);
+    if (timer == 100){
+        myEventEmitter.emit('make-a-order', { coffeeName: 'Arabica', coffeePrice: allCoffeePrice });
+    }
+}
